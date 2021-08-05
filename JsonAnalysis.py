8a5ab8db9636cd6c425dad5051d03aba9e75f682
Cesar-Colina <ccolina@ualberta.ca>
2021-08-04 19:00:06 -0600

    inserts desired state statistics into an SQLite database

--- JsonAnalysis.py
+++ JsonAnalysis.py
@@ -1,34 +1,65 @@
 #United States COVID-19 Cases and Deaths by State over Time database API
+#
 import json
 import urllib.request, urllib.parse, urllib.error
+import time
+import sqlite3
 
-apiurl = 'https://data.cdc.gov/resource/9mfq-cb36.json'
+#create a table to store all of the data
+connect = sqlite3.connect('CovidStatsdb.sqlite')
+cur = connect.cursor()
 
-print('accessing url: ' , apiurl)
-data = urllib.request.urlopen(apiurl)
-data = data.read().decode()
-print(len(data), ' characters retrieved')
 
-try:
-    js = json.loads(data)
-    print(len(js), ' length of list')
-except:
-    js = None
+cur.execute('''CREATE TABLE IF NOT EXISTS Stats (id FLOAT PRIMARY KEY, state_id FLOAT, submission_date TEXT,
+total_deaths FLOAT, new_deaths FLOAT, total_cases FLOAT, new_cases FLOAT, population FLOAT)''')
+cur.execute('''CREATE TABLE IF NOT EXISTS States(id FLOAT NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, name TEXT)''')
 
-if not js:
-    print("*****************failure to retrieve******************")
+url = 'https://data.cdc.gov/resource/9mfq-cb36.json'
+# ?state=STATE&$order=submission_date
+# link format to access the API and grab info for a specific date in order of every day since the pandemic
 
 NewYork = dict()
+iterate = dict()
+states = ['NYC', 'CA', 'FL', 'TX', 'SD']
+num = 1
+# 1, 2, 3, 4, 5
+# key values for the states
 
 
-for count in range(1000):
-    #print(js[count]['state'])
-    if js[count]['state'] == 'NYC':
-        #print(count, " hi")
-        #print(js[count]['submission_date'])
-        NewYork[js[count]['submission_date']] = NewYork.get(js[count]['submission_date'], js[count]['tot_cases'])
+for state in states:
 
-print(NewYork)
+    cur.execute('''INSERT INTO States (name) VALUES (?)''', (state,))
+    apiurl = url + '?state=' + state + '&$order=submission_date'
+    #create the link for the approriate state
 
-#print(js)
-#print(json.dumps(js, indent = 4))
+    print('accessing url: ' , apiurl)
+    data = urllib.request.urlopen(apiurl)
+    data = data.read().decode()
+    print(len(data), ' characters retrieved')
+
+    try:
+        js = json.loads(data)
+        print(len(js), ' length of list')
+    except:
+        js = None
+    if not js:
+        print("*****************failure to retrieve******************")
+        #error message if data fails to load
+
+
+    for count in range(len(js)):
+        if js[count]['state'] == state:
+            subd = js[count]['submission_date']
+            totd = js[count]['tot_death']
+            newd = js[count]['new_death']
+            totc = js[count]['tot_cases']
+            newc = js[count]['new_case']
+
+            cur.execute('''INSERT INTO Stats (state_id, submission_date, total_deaths, new_deaths, total_cases, new_cases)
+            VALUES (?, ?, ?, ?, ?, ?)''',(num, subd, float(totd), float(newd), float(totc), float(newc), ))
+
+    connect.commit()
+    #commit after each state
+    #import a time delay to avoid throttling
+    time.sleep(1.5)
+    num = num + 1
