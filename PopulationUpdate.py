8372e0d70f2e35b8fc0bbbea441c409d602ba504
Cesar-Colina <77550895+Cesar-Colina@users.noreply.github.com>
2021-08-19 20:23:55 -0600

    This section was uncessary. Simply wanted to practice bs4

--- PopulationUpdate.py
+++ PopulationUpdate.py
@@ -9,31 +9,28 @@ populations = dict()
 
 popurl = urllib.request.urlopen('https://worldpopulationreview.com/states').read()
 soup = bs4.BeautifulSoup(popurl, 'html.parser')
-# print(soup)
+
 
 tags = soup('li')
 #grab the population from the desired states
 for tag in tags:
-    #print(tag)
-    #print('URL:', tag.get('href', None))
-    #print('Contents:', tag.contents[0])
-    #print('Population: ', tag.get_text())
-    #print(re.findall('^\S+', tag.get_text()))
+
     for state in states:
-        if re.findall('^\S+', tag.get_text())[0] == state or re.findall('^\S+.\S+', tag.get_text())[0] == state:
+        if re.findall('^\S+', tag.get_text())[0] == state or \
+        re.findall('^\S+.\S+', tag.get_text())[0] == state:
             #find the states and put the popluation value in a dictionary
             try:
                 people = re.findall('[0-9]+,[0-9]+,[0-9]+', tag.get_text())[0]
+                #more than 6 digits
             except:
                 people = re.findall('[0-9]+,[0-9]+', tag.get_text())[0]
+                #6 digits or less
 
             # remove the comma's and store as an integer
             tempL = people.split(',')
-            #print(tempL)
             storage = ''
             for index in range(len(tempL)):
                 storage = storage + tempL[index]
-                #print(storage)
 
             populations[state] = populations.get(state, int(storage))
 
